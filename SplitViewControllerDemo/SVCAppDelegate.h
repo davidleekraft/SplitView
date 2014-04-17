d272710738ac305390418c25ea363b58ec877359
David Kraft <kraftd@goldmail.etsu.edu>
2014-04-16 20:03:17 -0400

    Initial Commit

---
+++ SplitViewControllerDemo/SVCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVCAppDelegate.h
+//  SplitViewControllerDemo
+//
+//  Created by David Kraft on 4/16/14.
+//  Copyright (c) 2014 David Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SplitViewControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SplitViewControllerDemo
+//
+//  Created by David Kraft on 4/16/14.
+//  Copyright (c) 2014 David Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SVCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVCAppDelegate class]));
+    }
+}
